eb74a9bcfed81ec480e645813b2c4a7fa77c2b40
big-xiao <1013415865@qq.com>
2018-09-10 08:45:28 +0800

    attempt

---
+++ app/src/main/java/com/example/administrator/qrcode_machine/Constant.java
@@ -0,0 +1,11 @@
+package com.example.administrator.qrcode_machine;
+
+/**
+ * Created by Administrator on 2018/8/30.
+ */
+
+ class Constant {
+    static final int REQ_PERM_CAMERA=1;
+    static final int REQ_QR_CODE=2;
+    static final String INTENT_EXTRA_KEY_QR_SCAN="result";
+}
